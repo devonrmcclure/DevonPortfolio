ce1efbb17b824fb34ac8d0d2e721445466c0f6ae
Phinocio <devon.r.mcclure@gmail.com>
2014-08-19 21:29:44 -0700

    Updated design of main page and added social media buttons

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -1,35 +1,48 @@
+<!--
+    Social media icons by Dawid Dapszus  https://dribbble.com/shots/1209419-20-Social-Media-Icons-Freebie
+ -->
+
 @extends ('layouts.base')
 
 @section('content')
 
 <div class="container content">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>
-                About Me
-            </h1>
-        </div>
-    </div>
 
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-3">
+            <img src="http://placehold.it/200x200" class="img-responsive center-block" />
             <p class="text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Hi there. My name is Devon McClure. I am an aspiring Web Developer living in White Rock, British Columbia. I am currently working on a website for my current place of employment, Batteries Included.
             </p>
         </div>
 
-        <div class="col-md-4">
-            <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="col-md-9">
+            <h1>Want to contact me?</h1>
+            <p class="text">
+                I am able to be contacted through multiple different websites. Feel free to contact me in the way that suits you best.
+            </p>
+
+            <div class="contact-icons">
+                <ul>
+                    <li>
+                        <a href="https://twitter.com/DevonRMcClure">
+                        <img src="{{ URL::asset('images/twitter.png') }}" class="img-responsive" alt="Twitter"/>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="https://ca.linkedin.com/pub/devon-mcclure/a1/b02/a10/">
+                        <img src="{{ URL::asset('images/linkedin.png') }}" class="img-responsive" alt="Linked In"/>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="https://github.com/Phinocio">
+                        <img src="{{ URL::asset('images/Github.png') }}" class="img-responsive" alt="Github"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
