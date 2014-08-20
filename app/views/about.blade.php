721777371a57071b46eb4c69056c463a4a2d1064
Phinocio <devon.r.mcclure@gmail.com>
2014-08-20 15:18:10 -0700

    Updated design of about page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -9,17 +9,27 @@
 <div class="container content">
 
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-4">
             <img src="http://placehold.it/200x200" class="img-responsive center-block" />
             <p class="text">
-                Hi there. My name is Devon McClure. I am an aspiring Web Developer living in White Rock, British Columbia. I am currently working on a website for my current place of employment, Batteries Included.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
         </div>
 
-        <div class="col-md-9">
+        <div class="col-md-8">
             <h1>Want to contact me?</h1>
             <p class="text">
-                I am able to be contacted through multiple different websites. Feel free to contact me in the way that suits you best.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
 
             <div class="contact-icons">
