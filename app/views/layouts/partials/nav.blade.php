d278c777d667f3c446a3662ce41882fe84fb02c9
Devon McClure <devon.r.mcclure@gmail.com>
2014-09-08 14:27:42 -0700

    Fixed navbar

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -1,18 +1,19 @@
-      <!-- Site header and navigation -->
-      <header class="top" role="header">
-        <div class="container">
-        <a href="http://devonmcclure.dev" class="navbar-brand pull-left">
-          Devon McClure
-        </a>
-          <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-           <span class="glyphicon glyphicon-align-justify"></span>
-          </button>
-          <nav class="navbar-collapse collapse navbar-right" role="navigation">
-            <ul class="nav navbar-nav">
-              <li><a id="home" href="about"><span class="glyphicon glyphicon-user"></span> About</a></li>
-              <li><a id="catalog" href="resume"><span class="glyphicon glyphicon-list-alt"></span> Resume</a></li>
-              <li><a id="servicing" href="portfolio"><span class="glyphicon glyphicon-folder-open"></span> Portfolio</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+<!-- Site header and navigation -->
+<div class="navbar navbar-inverted top navbar-fixed-top top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="glyphicon glyphicon-align-justify"></span>
+      </button>
+      <a class="navbar-brand" href="/">Devon McClure</a>
+    </div>
+    <div class="navbar-collapse collapse navbar-right">
+      <ul class="nav navbar-nav">
+        <li><a id="about" href="about"><span class="glyphicon glyphicon-user"></span> About</a></li>
+        <li><a id="resume" href="resume"><span class="glyphicon glyphicon-list-alt"></span> Resume</a></li>
+        <li><a id="portfolio" href="portfolio"><span class="glyphicon glyphicon-folder-open"></span> Portfolio</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
