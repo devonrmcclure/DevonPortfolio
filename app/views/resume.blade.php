3c498c8d2323973cb74f53df9b57ef0b80cb897c
Devon McClure <devon.r.mcclure@gmail.com>
2014-09-08 13:22:38 -0700

    Created portfolio page

---
+++ app/views/resume.blade.php
@@ -0,0 +1,99 @@
+<!--
+    Social media icons by Dawid Dapszus  https://dribbble.com/shots/1209419-20-Social-Media-Icons-Freebie
+ -->
+
+@extends ('layouts.base')
+
+@section('content')
+
+<div class="container content">
+
+<div class="col-md-2"></div>
+<div class="col-md-8 resume">
+
+    <div class="row">
+        <h2>Skills</h2>
+        <div class="col-md-6">
+            <h3>Programming</h3>
+            <ul>
+                <li>PHP & MySQL</li>
+                <li>HTML & JavaScript</li>
+                <li>Java</li>
+                <li>Less CSS</li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h3>Programming Technologies</h3>
+            <ul>
+                <li>Laravel 4</li>
+                <li>Composer</li>
+                <li>Bower</li>
+                <li>MVC Frameworks</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <h2>Experience</h2>
+        <div class="col-md-6">
+            <h3>Sales Associate</h3>
+            <p>November 2013 - Present</p>
+            <p>Batteries Included/Kata King Services</p>
+            <ul>
+                <li>Recreated store website</li>
+                <li>Replace watch batteries</li>
+                <li>Small appliance repair and servicing</li>
+                <li>Shaver repair and servicing</li>
+                <li>Sales</li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h3>Laborer</h3>
+            <p>July 2013 - October 2013</p>
+            <p>Servicemaster Restore</p>
+            <ul>
+                <li>Assist with demo, contents, etc</li>
+                <li>Part-time temp worker</li>
+                <li>Assisted in warehouse moving contents and general cleaning</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+        <h3>Dishwasher</h3>
+        <p>August 2012 - January 2014</p>
+        <p>De Dutch Fleetwood</p>
+        <ul>
+            <li>Ensure the cleanliness of dishes in a timely and complete manner</li>
+            <li>Assist cooks with prep work</li>
+            <li>Bus tables when necessary</li>
+            <li>Maintain cleanliness of the dishwashing area</li>
+        </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <h2>Education</h2>
+        <div class="col-md-6">
+            <h3>Kwantlen Polytechnic University</h3>
+            <ul>
+                <li>Bachelors of IT</li>
+                <li>September 2012 - September 2013</li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h3>Earl Marriott Secondary</h3>
+            <ul>
+                <li>Graduating class of 2012</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <p>References available upon request</p>
+    </div>
+
+</div>
+<div class="col-md-2"></div>
+</div>
+@stop
