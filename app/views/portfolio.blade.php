616efd51c43b0cd536fd5e9e69b9449047f717f2
Devon McClure <devon.r.mcclure@gmail.com>
2014-09-08 13:22:28 -0700

    Created portfolio page

---
+++ app/views/portfolio.blade.php
@@ -0,0 +1,17 @@
+<!--
+    Social media icons by Dawid Dapszus  https://dribbble.com/shots/1209419-20-Social-Media-Icons-Freebie
+ -->
+
+@extends ('layouts.base')
+
+@section('content')
+
+<div class="container content">
+
+<div class="col-md-2"></div>
+<div class="col-md-8">
+    <h3>Page under construction. Please check back shortly!</h3>
+</div>
+<div class="col-md-2"></div>
+</div>
+@stop
