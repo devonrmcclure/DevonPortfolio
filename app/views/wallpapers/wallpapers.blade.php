41ec8a0a1cdb28f62435e0cff0d831d8002fff5c
Devon McClure <devon.r.mcclure@gmail.com>
2015-11-10 13:46:07 -0800

    Added random wallpapers section for setting a random wallpaper on my phone VIA Tasker

---
+++ app/views/wallpapers/wallpapers.blade.php
@@ -0,0 +1,13 @@
+<!-- Page for randomly setting a wallpaper for my phone VIA Tasker
+
+Earth images from https://earthview.withgoogle.com/
+Pokemon images from https://imgur.com/gallery/2dup2 -->
+
+<html>
+<title>Random Wallpaper</title>
+
+<body>
+	<img src="{{$image}}"/>
+</body>
+
+</html>
